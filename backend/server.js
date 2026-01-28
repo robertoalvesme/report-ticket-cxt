3fb103f859000fbf61d44ec734f999d7456a255d
Roberto Alves <alves153@avaya.com>
2026-01-28 13:50:49 -0300

    Inicializar frontend

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ app.get('/api/dashboard', (req, res) => apiController.getDashboardData(req, res)
 // Rota de Healthcheck
 app.get('/', (req, res) => res.send('OCD Dashboard API Running'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
---
+++ frontend/app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <nuxt-page />
+  </main>
+</template>
